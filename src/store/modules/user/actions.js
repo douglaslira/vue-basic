ca5f973ae0046e12980293944205dbd02ea64766
Douglas Lira <douglas.lira.web@gmail.com>
2020-10-14 18:14:22 +0100

    [TASK][VB-002] - Implement remove iten

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -1,9 +1,16 @@
 import * as constants from './constants'
 import { state } from './state'
 
+const listOfUsers = [
+  { id: 1, name: 'Douglas Lira' },
+  { id: 2, name: 'Miguel Lira' },
+  { id: 3, name: 'Rafael Lira' },
+  { id: 4, name: 'Cibelly Lira' }
+]
+
 export const actions = {
   getAllUsers ({ commit }) {
-    commit(constants.SHOW_ALL_USER, [{ id: 1, name: 'Douglas Lira' }])
+    commit(constants.SHOW_ALL_USER, listOfUsers)
   },
   saveUser (context, data, bool) {
     state.message = 'Data success has saved'
@@ -17,7 +24,9 @@ export const actions = {
     const conf = window.confirm('Are you sure delete this Data?')
     if (conf) {
       state.message = 'Data success has deleted'
-      context.dispatch('getAllUsers')
+			const position = listOfUsers.map((e) => { return e.id }).indexOf(iduser);
+			listOfUsers.splice(position, 1)
+			context.dispatch('getAllUsers')
     }
   }
 }
--- src/store/modules/user/mutations.js
+++ src/store/modules/user/mutations.js
@@ -4,5 +4,8 @@ import Vue from 'vue'
 export const mutations = {
   [constants.SHOW_ALL_USER] (state, users) {
     Vue.set(state, 'all', users)
+	},
+	[constants.DELETE_ONLY_USER] (state, users) {
+    console.log(state, users);
   }
 }
